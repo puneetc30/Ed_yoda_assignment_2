5c86fa4ce86bc516227b939c067a0a396056c29b
Puneet Chaudhary <96575257+puneetc30@users.noreply.github.com>
2022-07-24 23:32:59 +0530

    Update fun_with_lists_and_tuples.py

--- fun_with_lists_and_tuples.py
+++ fun_with_lists_and_tuples.py
@@ -1,7 +1,9 @@
 size = int(input("Enter the size of the list: "))
+# This will take input from user for the size of list.
 lst = []
 for i in range(size):
     lst1 = []
+    # then this will take input from user to add two elements in each tuple in the list of tuples.
     ele1 = int(input("Enter first element: "))
     lst1.append(ele1)
     ele2 = int(input("Enter second element: "))
